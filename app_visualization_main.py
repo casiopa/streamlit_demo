9aa4bcddc363209c4af4012e8f095e1bad9368a4
Ana Blanco <anablancodelgado@gmail.com>
2021-12-03 12:15:45 +0100

    Change visualization app details

--- app_visualization_main.py
+++ app_visualization_main.py
@@ -6,6 +6,11 @@ st.set_page_config(page_title='Streamlit Demo',
                    page_icon='https://www.google.com/s2/favicons?domain=www.imdb.com',
                    layout="wide")
 
+# Pone el radio-button en horizontal. Afecta a todos los radio button de una página.
+# Por eso está puesto en este que es general a todo
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+
 st.sidebar.image('images/800px-IMDB_Logo_2016.svg.png', width=200)
 st.sidebar.header('Público, crítica y taquilla en IMDb')
 st.sidebar.markdown('Análisis exploratorio de datos | Películas 2014 a 2019')
@@ -16,22 +21,20 @@ menu = st.sidebar.radio(
     ("Intro", "Data", "Relaciones entre variables", "Matrices de correlación"),
 )
 
-# Pone el radio-button en horizontal. Afecta a todos los radio button de una página.
-# Por eso está puesto en este que es general a todo
-st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 st.sidebar.markdown('---')
 st.sidebar.write('Ana Blanco | Julio 2021 anablancodelgado@gmail.com https://casiopa.github.io/')
 
+
 if menu == 'Intro':
-    set_home()
-    #pass
+    #set_home()
+    pass
 elif menu == 'Data':
-    set_data()
-    #pass
+    #set_data()
+    pass
 elif menu == 'Relaciones entre variables':
-    set_relations()
-    #pass
+    #set_relations()
+    pass
 elif menu == 'Matrices de correlación':
-    set_arrays()
-    #pass
+    #set_arrays()
+    pass
